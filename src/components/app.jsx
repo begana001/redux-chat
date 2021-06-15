f766b227ee4b17bcfe6734f2964293e5cac07e11
Begana Choi <begana001.jpg@gmail.com>
2021-06-15 20:07:52 +0200

    adjust app skeleton layout

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
 
 import ChannelList from '../containers/channel_list.jsx';
 import MessageList from '../containers/message_list.jsx';
@@ -8,7 +8,7 @@ import MessageList from '../containers/message_list.jsx';
 const App = () => {
   return (
     <div className="app row">
-      <h1 className="title"><FontAwesomeIcon icon={faCoffee} />Redux Chat</h1>
+      <h1 className="title">Redux Chat  {/*<FontAwesomeIcon icon={faComments} />*/}</h1>
       <ChannelList />
       <MessageList />
     </div>
--- src/containers/channel_list.jsx
+++ src/containers/channel_list.jsx
@@ -3,22 +3,32 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { selectChannel, fetchMessages } from '../actions';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons';
+
 class ChannelList extends Component {
   componentWillMount() {
     this.props.selectChannel(this.props.selectedChannel);
   }
 
   handleClick = (e) => {
-    this.props.selectChannel(e.currentTarget.innerHTML);
+    this.props.selectChannel(e.currentTarget.innerText);
+  }
+
+  handleActive = (channel) => {
+    if(channel === this.props.selectedChannel) {
+      return <p className="selected">></p>
+    }
   }
 
   renderChannel = (channel) => {
     return (
       <li
-        key={channel}
-        className={channel === this.props.selectedChannel ? "selected" : null }
-        onClick={this.handleClick}
+      key={channel}
+      className={channel === this.props.selectedChannel ? "channel selected" : "channel" }
+      onClick={this.handleClick}
       >
+        {this.handleActive(channel)}
         {channel}
       </li>
     );
@@ -28,6 +38,7 @@ class ChannelList extends Component {
     return (
       <div className="channels col-sm-3">
         <ul>
+          <h3 className="channels__title">channels</h3>
           {this.props.channels.map((channel) => this.renderChannel(channel))}
         </ul>
       </div>
