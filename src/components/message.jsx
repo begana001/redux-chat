924d7d895b33d72fd54f315ebffa471c2a25be52
Begana Choi <begana001.jpg@gmail.com>
2021-06-24 18:32:45 +0200

    adjust message created_at time  convention

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Message = (props) => {
+  const createdAt = props.message.created_at.split('T');
+  const date = createdAt[0];
+  const time = createdAt[1].split('.')[0];
   return (
     <div className="message">
       <div className="message__info">
         <p>{props.message.author}</p>
-        <p>{props.message.created_at}</p>
+        <p>{date} {time}</p>
       </div>
       <p className="message__content">{props.message.content}</p>
     </div>
--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -19,7 +19,7 @@ class MessageList extends Component {
   }
 
   componentDidMount() {
-    this.refresher = setInterval(this.fetchMessages, 3000);
+    this.refresher = setInterval(this.fetchMessages, 1000);
   }
 
   componentWillUnmount() {
